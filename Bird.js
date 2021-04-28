97f53f2e390ae9b92d596d2f2e4030cbd9cdd305
BVC2013 <69219494+BVC2013@users.noreply.github.com>
2021-04-28 19:56:47 -0400

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -3,22 +3,23 @@ class Bird extends BaseClass {
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
     this.smokeImage = loadImage("sprites/smoke.png");
-    this.trajectory = [];
+    this.trajectory =[];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+
     super.display();
-    console.log(this.body.velocity.x);
-    console.log(this.body.position.x);
-    if (this.body.velocity.x>1 & this.body.position.x>200){
-      
-      var position = [this.body.position.x,this.body.position.y];
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
-    for(var i=0;i<this.trajectory.length;i++){
-      image(this.smokeImage,this.trajectory[i][0],this.trajectory[i][1])
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
     }
   }
 }
--- Slingshot.js
+++ Slingshot.js
@@ -13,11 +13,10 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-attach(body)
-{
-    this.sling.bodyA=body;
-
-}
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
     fly(){
         this.sling.bodyA = null;
     }
@@ -28,21 +27,24 @@ attach(body)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
             
+            stroke(48,22,8);
             if(pointA.x < 220) {
                 strokeWeight(7);
                 line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
                 image(this.sling3,pointA.x -30, pointA.y -10,15,30);
             }
             else{
                 strokeWeight(3);
                 line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
                 image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
             }
            
             
+            pop();
         }
     }
     
